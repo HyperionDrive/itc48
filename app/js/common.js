f8339ab06a904ebfe8ca0b15e4026c324ec2da98
Сергей <rserg.work@gmail.com>
2017-05-02 00:42:41 +0300

    1.0
    Первый комит из дома

---
+++ app/js/common.js
@@ -0,0 +1,17 @@
+$(function() {
+
+	$('.partners-slider').slick({
+		infinite: true,
+	  slidesToShow: 3,
+	  slidesToScroll: 1,
+	  variableWidth: false
+	});
+
+	$('.useful-links-slider').slick({
+		infinite: true,
+	  slidesToShow: 3,
+	  slidesToScroll: 1,
+	  variableWidth: false
+	});
+
+});

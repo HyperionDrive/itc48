1dc8e2faf18b74fc36ed8b0d0f25767c8eb15722
Сергей <rserg.work@gmail.com>
2017-05-15 17:06:05 +0300

    1.0.7
    Загружаю json из google spread sheets

--- app/js/common.js
+++ app/js/common.js
@@ -1,18 +1,39 @@
 $(function() {
 
+	$('.header-slider').slick({
+		centerMode: true,
+		slidesToShow: 1,
+		variableWidth: true,
+		draggable: false,
+		focusOnSelect: false,
+		initialSlide: 0,
+		slidesToScroll: 1,
+		autoplay: true,
+		speed: 1000
+	});
+
 	$('.partners-slider').slick({
 		infinite: true,
-	  slidesToShow: 3,
-	  slidesToScroll: 1,
-	  variableWidth: true
+	  	slidesToShow: 3,
+	  	slidesToScroll: 1,
+	  	variableWidth: true
 	});
 
 	$('.useful-links-slider').slick({
 		infinite: true,
-	  slidesToShow: 3,
-	  slidesToScroll: 1,
-	  variableWidth: false
+	  	slidesToShow: 3,
+	  	slidesToScroll: 1,
+	  	variableWidth: false
 	});
+
+	$('.useful-links-slider').slick({
+		infinite: true,
+	  	slidesToShow: 3,
+	  	slidesToScroll: 1,
+	  	variableWidth: false
+	});
+
+	
 
 	//steps-snippet
 	(function(){
@@ -75,56 +96,74 @@ $(function() {
 	// start поиск по реестру сертификатов
 	(function () {
 		var elem = $("#searchResult");
-		var obj;
+		var obj= {};
 		var inputDate = $("#searchDate");
 		var searchOrg = $("#searchOrg");
 		var searchSert = $("#searchSert");
 
 		if(elem.length) {
-			$.ajax({
-			  method: "POST",
-			  url: "/include/sertificatesJson.php"
-			  // data: { name: "John", location: "Boston" }
-			})
-	  		.done(function( msg ) {
-		    	obj = JSON.parse(msg);
-		  	});
+			var feedurl_start = "https://spreadsheets.google.com/feeds/";
+		    var feedkey = "1Ds_JQqX5jrdVZVlGSpRoA3lgieH1VjoNJ4GZuvL6t90";
+		    var flag = 0;
+
+		    $.getJSON(feedurl_start+"worksheets/"+feedkey+"/public/basic?alt=json", function(sheetfeed) {
+		    	count = sheetfeed.feed.entry.length;
+		        $.each(sheetfeed.feed.entry, function(k_sheet, v_sheet){
+		            var sheeturl = v_sheet.link[0]["href"]+"?alt=json";
+		            sheeturl = sheeturl.replace("basic", "values");
+		            var sheet_title = v_sheet.content["$t"]; // to know which sheet you're dealing with
+		            $.getJSON(sheeturl, function(sheetjson){
+		                obj[k_sheet] = sheetjson.feed.entry;
+		                if (!--count) {
+			            	flag = 1;
+			            	console.log(obj);
+			            	$.each([inputDate, searchOrg, searchSert], function () {
+			            		
+			            		$(this).on('blur', function(){
+			            			var field = $(this).data('field');
+			            			var value = $(this).val().trim().toLowerCase();
+
+			            			if(!value){
+			            				return false;
+			            			}
+			            			var result = doSearch(obj, field, value);
+
+			            			console.log(result);
+			            			if(!result.root.length){
+			            				elem.html('Ничего не найдено!');
+			            				return false
+			            			}
+
+			            			if(result.root.length > 20){
+			            				elem.html('Уточните поиск');
+			            				return false
+			            			}
+
+
+
+			            			 htm = Defiant.render('books_template', result);
+			            			 elem.html(htm);
+
+			            		});
+			            	});
+		        		}
+		            });
+		            
+		        });
+		    });
 		}
 
-		$.each([inputDate, searchOrg, searchSert], function () {
-			
-			$(this).on('blur', function(){
-				var field = $(this).data('field');
-				var value = $(this).val().trim().toLowerCase();
+		
 
-				if(!value){
-					return false;
-				}
-				var result = doSearch(obj, field, value);
-
-				console.log(result);
-				if(!result.root.length){
-					elem.html('Ничего не найдено!');
-					return false
-				}
-
-				if(result.root.length > 20){
-					elem.html('Уточните поиск');
-					return false
-				}
-
-
-
-				 htm = Defiant.render('books_template', result);
-				 elem.html(htm);
-
-			});
-		});
+		
 		
 
 		
 	})();
 	// end поиск по реестру сертификатов
+
+
+
 
 
 });
